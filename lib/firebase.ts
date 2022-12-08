00042d4712ff6d9ba6015f8057461384a52e5284
Alexis Solano <dev@alexisdev.me>
2022-12-07 19:57:41 -0600

    Remove old API keys

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,15 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5N-d9CJX5MZNrUXipvRtg9d-10gSeWfw",
-  authDomain: "firbase-next-tutorial.firebaseapp.com",
-  projectId: "firbase-next-tutorial",
-  storageBucket: "firbase-next-tutorial.appspot.com",
-  messagingSenderId: "342781454785",
-  appId: "1:342781454785:web:e41f340ed653396d63e740",
+  // This is a hack, because for some reason that I'm not fully understand, if you remove the concatenation from the env variables
+  // you will get an error with the API key, but if you console log this object (without the concatenation) and put it directally in the function call it will work!!
+  // JS in a nutshell :)
+  apiKey: `${process.env.FB_API_KEY}`,
+  authDomain: `${process.env.FB_AUTH_DOMAIN}`,
+  projectId: `${process.env.FB_PROJECT_ID}`,
+  storageBucket: `${process.env.FB_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.FB_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.FB_APP_ID}`,
 };
 
 if (!firebase.apps.length) {
